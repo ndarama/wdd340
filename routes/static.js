3e53472a800761fab15679a3aa3015f79a9aa4c5
ndarama <ndaramartwork@gmail.com>
2025-05-12 19:57:54 +0200

    added pages

--- routes/static.js
+++ routes/static.js
@@ -8,7 +8,9 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+//Index route
+app.get("/", function(req, res) {
+    res.render("index", {title: "Home"});
+});
+
 module.exports = router;
-
-
-
